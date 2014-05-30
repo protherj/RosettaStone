89e3ed0405d4db62d6de70b5e603d733472b57b0
Rusty Swayne <rusty@mindfly.com>
2014-05-29 19:52:57 -0700

    Integrated bootstrap heroic theme

---
+++ src/website/App_Code/RosettaHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+/// <summary>
+/// Summary description for RosettaHelper
+/// </summary>
+public class RosettaHelper
+{
+    /// <summary>
+    /// Utility to create html snippet to describe the views used on the current page
+    /// </summary>
+	public static MvcHtmlString GetViewBoxHtml(string viewName, string description)
+	{
+        const string html = @"<div class=""list-group-item"">
+                    <h3 class=""list-group-item-heading"">{0}</h3>
+                    <p class=""list-group-item-text"">{1}</p>    
+                </div>";
+
+	    return MvcHtmlString.Create(string.Format(html, viewName, description));
+	}
+}
